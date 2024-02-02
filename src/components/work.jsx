a43d6fe7cd58ebb511fe8f4e01ddd4ea89980dde
Tanveerchana <tanveer0736@gmail.com>
2024-02-02 18:46:03 +0530

    g

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -6,16 +6,16 @@ export default function Work() {
     return (
         <>
             <div className="work-section">
-                <div className="work-video">
+                {/* <div className="work-video">
                     <img src={capture} alt="" srcset="" width="100%" height="100%" />
-                </div>
+                </div> */}
                 <div className="work-title">
                     <h1>I craft unique solutions, listening, observing, creating, and validating with a human touch for efficient results, redefining digital experiences.</h1>
                     <Link to={"/work"}><button>Projects</button></Link>
                 </div>
-                <div className="work-video">
+                {/* <div className="work-video">
                     <img src={outfit} alt="" srcset="" width="100%" height="100%" />
-                </div>
+                </div> */}
             </div></>
     );
 }
